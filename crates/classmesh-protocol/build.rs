6b740e893be620a81bbc8b685edd3fca5a44664e
Ahmed <al-faris-7@hotmail.com>
2026-09-18 16:20:43 +0300

    style: apply rustfmt to protobuf build script

--- crates/classmesh-protocol/build.rs
+++ crates/classmesh-protocol/build.rs
@@ -4,10 +4,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let protoc = protoc_bin_vendored::protoc_bin_path()?;
     let mut config = prost_build::Config::new();
     config.protoc_executable(protoc);
-    config.compile_protos(
-        &["../../proto/classmesh_control.proto"],
-        &["../../proto"],
-    )?;
+    config.compile_protos(&["../../proto/classmesh_control.proto"], &["../../proto"])?;
     println!("cargo:rerun-if-changed=../../proto/classmesh_control.proto");
     Ok(())
 }
